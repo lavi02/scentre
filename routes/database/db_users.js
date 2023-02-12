32b1f7cef1497b72abb4e38428b38340cfd67b07
lavi02 <18ds10535@dshs.kr>
2023-02-12 16:28:00 +0900

    [CHANGE] DB_USER / USERS CODES

--- routes/database/db_users.js
+++ routes/database/db_users.js
@@ -1,8 +1,8 @@
 const db_data = require('../../extra/mg_collection');
 
-exports.userStatus_get = async (id, detail) => {
+exports.userStatus_get = async (id) => {
     await db_data.scentre_user_data.find(
-        {'id': id}.exec((err, data) => { 
+        { 'id': id }.exec((err, data) => { 
             if (!err) 
                 return data;
             else return err; 
@@ -12,64 +12,6 @@ exports.userStatus_get = async (id, detail) => {
 
 exports.userStatus_post = (req) => {
     const user_data = new db_data.scentre_user_data(
-        {
-            type: req.type,
-            id: req.id,
-            name: req.name,
-            password: req.password,
-            set_date: req.set_date,
-            gender: req.gender,
-            ph_number: req.ph_number,
-            br_number: req.br_number,
-            bs_category: req.bs_category,
-            bs_type: req.bs_type,
-            address: req.address,
-            addr_detail: req.addr_detail,
-            email: req.email,
-            point: 0,
-            times_of_order: 0,
-            status: 1,
-            register_date: Date.now()
-        })
-
-        user_data.save().then(
-            () => {
-                return 0;
-            }
-        ).catch((err) => { return err; });
-}
-
-exports.userStatus_put = (req) => {
-    const user_data = new db_data.scentre_user_data(
-        {
-            type: req.type,
-            id: req.id,
-            name: req.name,
-            password: req.password,
-            set_date: req.set_date,
-            gender: req.gender,
-            ph_number: req.ph_number,
-            br_number: req.br_number,
-            bs_category: req.bs_category,
-            bs_type: req.bs_type,
-            address: req.address,
-            addr_detail: req.addr_detail,
-            email: req.email,
-            point: 0,
-            times_of_order: 0,
-            status: 1,
-            register_date: Date.now()
-        })
-
-        user_data.save().then(
-            () => {
-                return 0;
-            }
-        ).catch((err) => { return err; });
-}
-
-exports.userStatus_delete = async (req) => {
-    await db_data.scentre_user_data.update(
         {
             "type": req.type,
             "id": req.id,
@@ -88,6 +30,64 @@ exports.userStatus_delete = async (req) => {
             "times_of_order": 0,
             "status": 1,
             "register_date": Date.now()
+        })
+
+        user_data.save().then(
+            () => {
+                return 0;
+            }
+        ).catch((err) => { return err; });
+}
+
+exports.userStatus_put = (req) => {
+    const user_data = new db_data.scentre_user_data(
+        {
+            "type": req.type,
+            "id": req.id,
+            "name": req.name,
+            "password": req.password,
+            "set_date": req.set_date,
+            "gender": req.gender,
+            "ph_number": req.ph_number,
+            "br_number": req.br_number,
+            "bs_category": req.bs_category,
+            "bs_type": req.bs_type,
+            "address": req.address,
+            "addr_detail": req.addr_detail,
+            "email": req.email,
+            "point": 0,
+            "times_of_order": 0,
+            "status": 1,
+            "register_date": Date.now()
+        })
+
+        user_data.save().then(
+            () => {
+                return 0;
+            }
+        ).catch((err) => { return err; });
+}
+
+exports.change_userStatus_rank = async (req) => {
+    await db_data.scentre_user_data.update(
+        {
+            "id": req.id,
+            "name": req.name,
+        },
+        { "status": req.status }
+    ).then(
+        () => {
+            return 0;
+        }
+    ).catch((err) => { return err; })
+}
+
+exports.userStatus_delete = async (req) => {
+    await db_data.scentre_user_data.update(
+        {
+            "type": req.type,
+            "id": req.id,
+            "name": req.name
         }
     ).then(
         () => {
